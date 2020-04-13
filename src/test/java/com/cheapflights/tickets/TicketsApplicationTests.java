4a9d0a143440efcb1b2a7a6bfa35b9817ac667ae
Milan Deket <milandeket@gmail.com>
2020-04-13 20:15:10 +0200

    Initial SpringBoot app.

---
+++ src/test/java/com/cheapflights/tickets/TicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheapflights.tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
