f6011708fa1c499159a26c833a6979db6185ca02
Milan Deket <milandeket@gmail.com>
2020-04-13 21:33:38 +0200

    Added configuration for H2.

---
+++ src/main/resources/db/migration/V1_0__user_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS user(
+   id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   first_name varchar(30) NOT NULL,
+   last_name varchar(50) NOT NULL,
+   email varchar(50) NOT NULL,
+   username varchar(50) NOT NULL,
+   password varchar(128) NOT NULL,
+   salt varchar(100) NOT NULL,
+   role varchar(20) NOT NULL
+)
