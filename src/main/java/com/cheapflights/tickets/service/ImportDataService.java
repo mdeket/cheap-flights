66bcb8cfaa9107083bf189f94bba66423743e7c2
Milan Deket <milandeket@gmail.com>
2020-04-15 19:48:24 +0200

    Parsing airports and apache.commons added to pom.

--- src/main/java/com/cheapflights/tickets/service/ImportDataService.java
+++ src/main/java/com/cheapflights/tickets/service/ImportDataService.java
@@ -1,18 +1,24 @@
 package com.cheapflights.tickets.service;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.LineIterator;
+import lombok.extern.java.Log;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 @Service
+@Log
 public class ImportDataService implements CommandLineRunner {
 
     public void loadAirports() {
+        log.info("Loading airports...");
         File file = null;
         try {
             file = ResourceUtils.getFile("classpath:airports.txt");
@@ -20,22 +26,31 @@ public class ImportDataService implements CommandLineRunner {
             e.printStackTrace();
         }
 
-        if(file == null) {
+        if (file == null) {
             throw new RuntimeException("Airports.txt can't be found in resource folder.");
         }
 
-        try (LineIterator it = FileUtils.lineIterator(file, "UTF-8")) {
-            while (it.hasNext()) {
-                String line = it.nextLine();
-                System.out.println(line);
-            }
+        CSVParser parser = null;
+        try {
+            parser = CSVParser.parse(file, Charset.defaultCharset(), CSVFormat.ORACLE);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        if (parser == null) {
+            throw new RuntimeException("Could'n instantiate csv parser.");
+        }
+
+        for (CSVRecord csvRecord : parser) {
+            System.out.println(csvRecord);
+        }
+
+        log.info("Successfully loaded airports.");
     }
 
     @Override
     public void run(String... args) {
+        log.info("Started command line runner.");
         loadAirports();
+
     }
 }
