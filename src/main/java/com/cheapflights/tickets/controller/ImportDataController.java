33721d6e930a514f8116e6f7fa44af65a455f761
Milan Deket <milandeket@gmail.com>
2020-04-22 17:57:29 +0200

    Improved async exception handling.

--- src/main/java/com/cheapflights/tickets/controller/ImportDataController.java
+++ src/main/java/com/cheapflights/tickets/controller/ImportDataController.java
@@ -2,6 +2,7 @@ package com.cheapflights.tickets.controller;
 
 import com.cheapflights.tickets.domain.dto.ResponseMessageDTO;
 import com.cheapflights.tickets.service.ImportDataService;
+import lombok.extern.java.Log;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,10 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 
 @RestController
 @RequestMapping("/api/v1/import")
+@Log
 public class ImportDataController {
 
     private final ImportDataService importDataService;
@@ -27,7 +29,8 @@ public class ImportDataController {
     @PostMapping("/airports")
     public ResponseEntity<ResponseMessageDTO> uploadAirports(@RequestParam(name = "file") MultipartFile file) {
         File savedFile = importDataService.saveFile(file);
-        importDataService.loadAirportsAndCities(savedFile);
+        importDataService.loadAirportsAndCities(savedFile)
+                .exceptionally(this::asyncExceptionHandler);
         return ResponseEntity.ok(new ResponseMessageDTO("Parsing of " + file.getOriginalFilename() + " has started and it will take a couple of seconds up to a one minute [Async]."));
     }
 
@@ -35,7 +38,13 @@ public class ImportDataController {
     @PostMapping("/routes")
     public ResponseEntity<ResponseMessageDTO> uploadRoutes(@RequestParam("file") MultipartFile file) {
         File savedFile = importDataService.saveFile(file);
-        importDataService.loadRoutes(savedFile);
+        importDataService.loadRoutes(savedFile)
+                .exceptionally(this::asyncExceptionHandler);
         return ResponseEntity.ok(new ResponseMessageDTO("Parsing of " + file.getOriginalFilename() + " has started and it will take a couple of seconds up to a one minute [Async]."));
+    }
+
+    private Void asyncExceptionHandler(Throwable e) {
+        log.severe(e.getMessage());
+        return null;
     }
 }
